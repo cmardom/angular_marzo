acff217abb5051534a658736e2fd0a221bd50e7e
Carmen <cmardom599@g.educaand.es>
2024-03-15 12:16:42 +0100

    iinit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ConfiguracionComponent } from './components/configuracion/configuracion.component';
+import { CampingsComponent } from './components/campings/campings.component';
+import { CampingsFormularioComponent } from './components/campings/campings-formulario/campings-formulario.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'home', component: HomeComponent},
+    {path:'configuracion', component: ConfiguracionComponent},
+    {path:'campings', component: CampingsComponent},
+    {path:'campings/nuevo', component: CampingsFormularioComponent},
+    {path:'campings/:id', component: CampingsFormularioComponent},
+    {path:'**', pathMatch:'full', redirectTo: 'home'}
+];
